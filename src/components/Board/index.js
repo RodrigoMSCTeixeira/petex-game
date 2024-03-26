f63483089cd3cf414dbebc059f56c610ec1c6ec1
Rodrigo Teixeira <rteixerira@gmail.com>
2024-03-25 21:48:14 -0300

    update

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,18 +1,29 @@
 import React, { useEffect, useState } from "react";
-import styles from './Board.module.css';
+import styles from "./Board.module.css";
 import ControlController from "../../controllers/ControlController";
 import CheckValidController from "../../controllers/CheckValidController";
 import CreateBoardController from "../../controllers/CreateBoardController";
 
-const ballColors = ["blue", "orange", "green", "yellow", "red", "purple"]
-const rows = 6
-const columns = 6
-const boardElements = new CreateBoardController(ballColors, rows, columns).create
-const controls = new ControlController(ballColors, rows, columns, boardElements)
+const ballColors = ["blue", "orange", "green", "yellow", "red", "purple"];
+const rows = 7;
+const columns = 7;
+const boardElements = new CreateBoardController(ballColors, rows, columns)
+  .create;
+const controls = new ControlController(
+  ballColors,
+  rows,
+  columns,
+  boardElements
+);
 
 function Board() {
-  const [currentBall, setCurrentBall] = useState(null)
-  const [otherBall, setOtherBall] = useState(null)
+  const [currentBall, setCurrentBall] = useState(null);
+  const [currentBallId, setCurrentBallId] = useState(null);
+  const [currentBallColor, setCurrentBallColor] = useState(null);
+  const [otherBall, setOtherBall] = useState(null);
+  const [otherBallId, setOtherBallId] = useState(null);
+  const [otherBallColor, setOtherBallColor] = useState(null);
+  const [board, setBoard] = useState(boardElements);
 
   useEffect(() => {
     // setInterval(function () {
@@ -20,77 +31,116 @@ function Board() {
     //   const slideBall = controls.getSlideBall
     //   const generateBall = controls.getGenerateBall
     // }, 100)
+    //const crushBall = controls.getCrushBall;
+    //const slideBall = controls.getSlideBall;
+    //const generateBall = controls.getGenerateBall;
+  }, [currentBall, otherBall]);
 
-    const crushBall = controls.getCrushBall
-    const slideBall = controls.getSlideBall
-    const generateBall = controls.getGenerateBall
-
-  }, [currentBall])
-
-  function dragStart(e) {
-    setCurrentBall(e.target.id)
+  function dragStart(e, ball) {
+    setCurrentBall(ball);
+    setCurrentBallId(e.target.children[0].id);
+    setCurrentBallColor(e.target.firstChild.classList[1]);
   }
 
   function dragOver(e) {
-    e.preventDefault()
+    e.preventDefault();
   }
 
   function dragEnter(e) {
-    e.preventDefault()
+    e.preventDefault();
   }
 
-  function dragLeave() {
-  }
+  function dragLeave() {}
 
-  function dragDrop(e) {
-    setOtherBall(e.target.id)
+  function dragDrop(e, ball) {
+    setOtherBallId(e.target.id);
+    setOtherBall(ball);
+    setOtherBallColor(e.target.classList[1]);
   }
-
 
   function dragEnd() {
-    const currentCoords = currentBall.split('-')
-    const currentBallRow = Number(currentCoords[0])
-    const currentBallColumn = Number(currentCoords[1])
-
-    const ohterCoords = otherBall.split('-')
-    const otherBallRow = Number(ohterCoords[0])
-    const otherBallColumn = Number(ohterCoords[1])
-
-    const moveLeft = otherBallColumn === currentBallColumn - 1 && currentBallRow === otherBallRow
-    const moveRight = otherBallColumn === currentBallColumn + 1 && currentBallRow === otherBallRow
-
-    const moveUp = otherBallRow === currentBallRow - 1 && currentBallColumn === otherBallColumn
-    const moveDown = otherBallRow === currentBallRow + 1 && currentBallColumn === otherBallColumn
-
-    const isAdjacent = moveLeft || moveRight || moveUp || moveDown
-
-    if (isAdjacent) {
-      const validMove = new CheckValidController(rows, columns, boardElements).handleCheckValid
-      setCurrentBall(otherBall)
-      setOtherBall(currentBall)
-
-      if (!validMove) {
-        setCurrentBall(otherBall)
-        setOtherBall(currentBall)
-      }
+    if (
+      currentBallColor.includes("white") ||
+      otherBallColor.includes("white")
+    ) {
+      return;
     }
 
+    const currentCoords = currentBallId.split("-");
+    const currentBallRow = Number(currentCoords[0]);
+    const currentBallColumn = Number(currentCoords[1]);
+
+    const ohterCoords = otherBallId.split("-");
+    const otherBallRow = Number(ohterCoords[0]);
+    const otherBallColumn = Number(ohterCoords[1]);
+
+    const moveLeft =
+      otherBallColumn === currentBallColumn - 1 &&
+      currentBallRow === otherBallRow;
+    const moveRight =
+      otherBallColumn === currentBallColumn + 1 &&
+      currentBallRow === otherBallRow;
+
+    const moveUp =
+      otherBallRow === currentBallRow - 1 &&
+      currentBallColumn === otherBallColumn;
+    const moveDown =
+      otherBallRow === currentBallRow + 1 &&
+      currentBallColumn === otherBallColumn;
+
+    const isAdjacent = moveLeft || moveRight || moveUp || moveDown;
+
+    if (isAdjacent) {
+      const newBoard = [...board];
+
+      console.log(currentBallId, otherBallId);
+
+      console.log(currentBall, otherBall);
+
+      for (let i = 0; i < newBoard.length; i++) {
+        for (let j = 0; j < newBoard[i].length; j++) {
+          if (newBoard[i][j].ball_id == currentBallId) {
+            newBoard[i][j] = otherBall;
+          }
+          if (newBoard[i][j].ball_id == otherBallId) {
+            newBoard[i][j] = currentBall;
+          }
+        }
+      }
+
+      const validMove = new CheckValidController(rows, columns, newBoard)
+        .handleCheckValid;
+
+      if (!validMove) {
+        for (let i = 0; i < newBoard.length; i++) {
+          for (let j = 0; j < newBoard[i].length; j++) {
+            if (newBoard[i][j].ball_id == currentBallId) {
+              newBoard[i][j] = otherBall;
+            }
+            if (newBoard[i][j].ball_id == otherBallId) {
+              newBoard[i][j] = currentBall;
+            }
+          }
+        }
+      }
+      setBoard(newBoard);
+    }
   }
 
   return (
     <div className={styles.board}>
-      {boardElements.map(item => (
-        item.map(ball => (
+      {board.map((item) =>
+        item.map((ball, i) => (
           <div
             className={styles.currentBall}
-            key={ball.id}
+            key={i}
             draggable
-            onDragStart={dragStart}
+            onDragStart={(e) => dragStart(e, ball)}
             onDragOver={dragOver}
             onDragEnter={dragEnter}
             onDragLeave={dragLeave}
             onDragEnd={dragEnd}
-            onDrop={dragDrop}
+            onDrop={(e) => dragDrop(e, ball)}
           >
             <div
               id={ball.ball_id}
@@ -98,11 +148,10 @@ function Board() {
               style={{ rotate: `${ball.ball_track_rotation}deg` }}
             ></div>
           </div>
-
         ))
-      ))}
+      )}
     </div>
-  )
+  );
 }
 
-export default Board
+export default Board;
--- src/services/CheckValidService/index.js
+++ src/services/CheckValidService/index.js
@@ -1,55 +1,53 @@
 class CheckValid {
-  #rows = 0
-  #colums = 0
-  #balls = []
+  #rows = 0;
+  #colums = 0;
+  #balls = [];
 
-  constructor({
-    rows = 0,
-    colums = 0,
-    balls = [] }) {
-    this.#rows = rows
-    this.#colums = colums
-    this.#balls = balls
+  constructor({ rows = 0, colums = 0, balls = [] }) {
+    this.#rows = rows;
+    this.#colums = colums;
+    this.#balls = balls;
   }
 
   #checkValid() {
     for (let i = 0; i < this.#rows; i++) {
       for (let j = 0; j < this.#colums - 2; j++) {
-        const ball1 = this.#balls[i][j]
-        const ball2 = this.#balls[i][j + 1]
-        const ball3 = this.#balls[i][j + 2]
+        let ball1 = this.#balls[i][j];
+        let ball2 = this.#balls[i][j + 1];
+        let ball3 = this.#balls[i][j + 2];
 
         if (
           ball1.ball_color === ball2.ball_color &&
           ball2.ball_color === ball3.ball_color &&
-          !ball1.ball_color.includes('white')) {
-          return true
+          !ball1.ball_color.includes("white")
+        ) {
+          return true;
         }
       }
     }
 
     for (let i = 0; i < this.#colums; i++) {
       for (let j = 0; j < this.#rows - 2; j++) {
-        const ball1 = this.#balls[j][i]
-        const ball2 = this.#balls[j + 1][i]
-        const ball3 = this.#balls[j + 2][i]
+        let ball1 = this.#balls[j][i];
+        let ball2 = this.#balls[j + 1][i];
+        let ball3 = this.#balls[j + 2][i];
 
         if (
           ball1.ball_color === ball2.ball_color &&
           ball2.ball_color === ball3.ball_color &&
-          !ball1.ball_color.includes('white')) {
-          return true
+          !ball1.ball_color.includes("white")
+        ) {
+          return true;
         }
       }
     }
 
-    return false
+    return false;
   }
 
   get handleCheckValid() {
-    return this.#checkValid()
+    return this.#checkValid();
   }
-
 }
 
-export default CheckValid
+export default CheckValid;
